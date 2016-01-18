18fc117d5305b83148d7ca4daedd84e4fdd3df58
Jan Klötzke <jan@kloetzke.net>
2016-01-18 09:32:27 +0100

    Add codecov.io support

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-cd "${0%/*}"
-export PYTHONPATH="${PWD}/../pym"
-python3 -m unittest
+
+cd "${0%/*}/.."
+export PYTHONPATH="${PWD}/pym"
+
+if [ -n "$(which coverage)" ] ; then
+	coverage run --source "./pym" -m unittest discover test
+else
+	python3 -m unittest discover test
+fi
