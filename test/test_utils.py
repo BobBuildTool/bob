9115d0f8f19c75c3194ccbf3b3b9ca59d7717ce5
Jan Klötzke <jan@kloetzke.net>
2016-01-21 20:38:23 +0100

    test_utils: add testcase for joinScripts()

---
+++ test/test_utils.py
@@ -0,0 +1,40 @@
+# Bob build tool
+# Copyright (C) 2016  Jan Klötzke
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from bob.utils import joinScripts
+
+class TestJoinScripts(TestCase):
+
+    def testEmpty(self):
+        assert joinScripts([]) == None
+
+    def testSingle(self):
+        assert joinScripts(["asdf"]) == "asdf"
+        assert joinScripts([None]) == None
+
+    def testDual(self):
+        s = joinScripts(["asdf", "qwer"]).splitlines()
+        assert "asdf" in s
+        assert "qwer" in s
+        assert s.index("asdf") < s.index("qwer")
+
+        assert joinScripts(["asdf", None]) == "asdf"
+        assert joinScripts([None, "asdf"]) == "asdf"
+
+        assert joinScripts([None, None]) == None
+
