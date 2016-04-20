589bdae4fa7a1c5f51680e60440607de37aea71b
Jan Klötzke <jan@kloetzke.net>
2016-04-20 22:36:12 +0200

    test: add test for string functions from plugins

---
+++ test/blackbox/string-plugin/plugins/override.py
@@ -0,0 +1,22 @@
+from bob.errors import ParseError
+
+# $(override,BUILD_TYPE_OVERRIDE,default)
+# expected format is "package_a:override1,package_b:override2,..."
+def override(args, env, recipe, **options):
+    if len(args) != 2:
+        raise ParseError("override expects two arguments")
+
+    if args[0] in env:
+        o = env[args[0]]
+        o = [ i.split(":") for i in o.split(",") ]
+        o = { k:v for (k,v) in o }
+        return o.get(recipe.getPackageName(), args[1])
+    else:
+        return args[1]
+
+manifest = {
+    'apiVersion' : "0.2",
+    'stringFunctions' : {
+        "override" : override
+    }
+}
