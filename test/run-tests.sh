43b764bc4a16076531a289acb7771f98db1cb650
Jan Klötzke <jan@kloetzke.net>
2017-02-12 19:52:51 +0100

    test: allow fine grained test execution
    Unit tests are executed per file instead of all together. This allows to
    see much better which tests are failing.
    
    Also introduce switched to selectively run some unit or black box tests.

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+set -o pipefail
+
+usage()
+{
+	cat <<EOF
+usage: ${0##*/} [-h] [-u PATTERN] [-b PATTERN] [-c]
+
+optional arguments:
+  -h              show this help message and exit
+  -u PATTERN      Only execute unit tests matching PATTERN
+  -b PATTERN      Only execute black box tests matching PATTERN
+  -c              Create HTML coverage report
+EOF
+}
+
+# move to root directory
 cd "${0%/*}/.."
 . ./test/test-lib.sh
 
 USE_COVERAGE=0
 FAILED=0
 RUN_TEST_DIRS=( )
+RUN_UNITTEST_PAT='*'
+RUN_BLACKBOX_PAT='*'
+GEN_HTML=0
 
 # check if python coverage is installed
 if type -fp coverage3 >/dev/null; then
@@ -21,39 +40,101 @@ else
 	RUN=python3
 fi
 
-# run unit tests
-echo "Run unit tests..."
+# option processing
+while getopts ":hcb:u:" opt; do
+	case $opt in
+		h)
+			usage
+			exit 0
+			;;
+		c)
+			GEN_HTML=1
+			;;
+		b)
+			RUN_UNITTEST_PAT=''
+			RUN_BLACKBOX_PAT="$OPTARG"
+			;;
+		u)
+			RUN_BLACKBOX_PAT=''
+			RUN_UNITTEST_PAT="$OPTARG"
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+	esac
+done
+
+# go to tests directory
 pushd test > /dev/null
-if ! $RUN -m unittest discover . ; then
-	: $((FAILED++))
-	echo "Some unit test(s) failed"
+
+# add marker to log.txt
+{
+	echo "######################################################################"
+	echo -n "Started: "
+	date
+	echo "Options: $*"
+} >> log.txt
+
+# run unit tests
+if [[ -n "$RUN_UNITTEST_PAT" ]] ; then
+	echo "Run unit tests..."
+	for i in test_*.py ; do
+		if [[ "${i%%.py}" == $RUN_UNITTEST_PAT ]] ; then
+			echo -n "   ${i%%.py} ... "
+			{
+				echo "======================================================================"
+				echo "Test: $i"
+			} >> log.txt
+			if $RUN -m unittest -v $i 2>&1 | tee log-cmd.txt >> log.txt ; then
+				echo "ok"
+			else
+				echo "FAIL (log follows...)"
+				: $((FAILED++))
+				cat -n log-cmd.txt
+			fi
+		fi
+	done
 fi
-popd > /dev/null
 
 # run blackbox tests
-echo "Run black box tests..."
-for i in test/* ; do
-	if [[ -d $i && -e $i/run.sh ]] ; then
-		RUN_TEST_DIRS+=( $i )
+if [[ -n "$RUN_BLACKBOX_PAT" ]] ; then
+	echo "Run black box tests..."
+	for i in * ; do
+		if [[ -d $i && -e "$i/run.sh" && "$i" == $RUN_BLACKBOX_PAT ]] ; then
+			RUN_TEST_DIRS+=( "test/$i" )
 
-		echo "   " $i
-		(
-			set -o pipefail
-			set -e
-			cd $i
-			. run.sh > log.txt
-		)
+			echo -n "   $i ... "
+			{
+				echo "======================================================================"
+				echo "Test: $i"
+			} >> log.txt
+			(
+				set -o pipefail
+				set -e
+				cd "$i"
+				. run.sh 2>&1 | tee log.txt
+			) | tee log-cmd.txt >> log.txt
 
-		if [[ $? -ne 0 ]] ; then
-			: $((FAILED++))
-			echo $i failed
+			if [[ $? -ne 0 ]] ; then
+				echo "FAIL (log follows...)"
+				: $((FAILED++))
+				cat -n log-cmd.txt
+			else
+				echo "ok"
+			fi
 		fi
-	fi
-done
+	done
+fi
+
+popd > /dev/null
 
 # collect coverage
 if [[ $USE_COVERAGE -eq 1 ]]; then
 	coverage3 combine test "${RUN_TEST_DIRS[@]}"
+	if [[ $GEN_HTML -eq 1 ]] ; then
+		coverage3 html
+	fi
 fi
 
 exit $FAILED
--- test/test-lib.sh
+++ test/test-lib.sh
@@ -36,12 +36,12 @@ exec_blackbox_test()
 {
 	cleanup
 
-	run_bob dev root > log-cmd.txt
-	RES=$(sed -ne '/^Build result is in/s/.* //p' log-cmd.txt)
+	run_bob dev root
+	RES=$(run_bob query-path -f '{dist}' --develop root)
 	diff -Nurp $RES output
 
-	run_bob build root > log-cmd.txt
-	RES=$(sed -ne '/^Build result is in/s/.* //p' log-cmd.txt)
+	run_bob build root
+	RES=$(run_bob query-path -f '{dist}' --release root)
 	diff -Nurp $RES output
 
 	run_bob clean
