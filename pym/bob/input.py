BobBuildTool/bob

commit 518ad759f925757509dfc9ddcd98eab7dc92d35a

input: fix endless loop in cyclic classes
Rework the class inheritance code to a recursive algorithm and add a
check to detect arbitrary cycles. Once a cycles is found stop parsing
the recipes and print the cycle in the error message.

Fixes #131.

pym/bob/input.py
@@ -1732,28 +1732,33 @@ class Recipe(object):
         # involved.
         self.__checkoutDeterministic = recipe.get("checkoutDeterministic", checkoutScript is None)
 
+    def __resolveClassesOrder(self, cls, stack, visited):
+        # prevent cycles
+        clsName = cls.__packageName
+        if clsName in stack:
+            raise ParseError("Cyclic class inheritence: " + " -> ".join(stack + [clsName]))
+
+        # depth first
+        ret = []
+        subInherit = [ self.__recipeSet.getClass(c) for c in cls.__inherit ]
+        if cls.__anonBaseClass: subInherit.insert(0, cls.__anonBaseClass)
+        for c in subInherit:
+            ret.extend(self.__resolveClassesOrder(c, stack + [clsName], visited))
+
+        # classes are inherited only once
+        if clsName not in visited:
+            ret.append(cls)
+            visited.add(clsName)
+
+        return ret
+
     def resolveClasses(self):
         # must be done only once
         if self.__classesResolved: return
         self.__classesResolved = True
 
-        # calculate order of classes (depth first)
-        visited = set()
-        backlog = [ self.__recipeSet.getClass(c) for c in self.__inherit ]
-        if self.__anonBaseClass: backlog.insert(0, self.__anonBaseClass)
-        inherit = []
-        while backlog:
-            next = backlog.pop(0)
-            if next.__packageName in visited: continue
-            subInherit = [ self.__recipeSet.getClass(c) for c in next.__inherit if c not in visited ]
-            if next.__anonBaseClass and (next.__anonBaseClass.__packageName not in visited):
-                subInherit.insert(0, next.__anonBaseClass)
-            if subInherit:
-                # prepend and re-insert current class
-                backlog[0:0] = subInherit + [next]
-            else:
-                inherit.append(next)
-                visited.add(next.__packageName)
+        # calculate order of classes (depth first) but ignore ourself
+        inherit = self.__resolveClassesOrder(self, [], set([self.__packageName]))
 
         # inherit classes
         inherit.reverse()

commit 921ec8bb6c785e9e479f5408b7740878bd1964e8

input: fix class parsing regression
Recipes and classes are different namespaces. 518ad759 introduced a
regression that prevented recipes from inheriting classes that have the
same name.

pym/bob/input.py
@@ -1734,9 +1734,9 @@ class Recipe(object):
         # involved.
         self.__checkoutDeterministic = recipe.get("checkoutDeterministic", checkoutScript is None)
 
-    def __resolveClassesOrder(self, cls, stack, visited):
+    def __resolveClassesOrder(self, cls, stack, visited, isRecipe=False):
         # prevent cycles
-        clsName = cls.__packageName
+        clsName = "<recipe>" if isRecipe else cls.__packageName
         if clsName in stack:
             raise ParseError("Cyclic class inheritence: " + " -> ".join(stack + [clsName]))
 
@@ -1748,7 +1748,7 @@ class Recipe(object):
             ret.extend(self.__resolveClassesOrder(c, stack + [clsName], visited))
 
         # classes are inherited only once
-        if clsName not in visited:
+        if (clsName not in visited) and not isRecipe:
             ret.append(cls)
             visited.add(clsName)
 
@@ -1760,7 +1760,7 @@ class Recipe(object):
         self.__classesResolved = True
 
         # calculate order of classes (depth first) but ignore ourself
-        inherit = self.__resolveClassesOrder(self, [], set([self.__packageName]))
+        inherit = self.__resolveClassesOrder(self, [], set(), True)
 
         # inherit classes
         inherit.reverse()