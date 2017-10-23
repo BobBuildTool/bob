BobBuildTool/bob

commit ce41dad99ceb42123ea35448cdcf51bbf4e7bf03

utils: make updateDicRecursive append lists
Amend updateDicRecursive to append lists instead of overwriting them.
Currently there is only one such list used for the 'always_checkout'
command option.

pym/bob/utils.py
@@ -68,12 +68,18 @@ def emptyDirectory(path):
     except OSError as e:
         raise BuildError("Error cleaning '"+path+"': " + str(e))
 
-# recursive update entries of a dictonary. See: http://stackoverflow.com/questions/3232943
+# Recursively merge entries of two dictonaries.
+#
+# Expect that both arguments have the same schema. Dictionaries are merged
+# key-by-key. Lists are appended. Returns merged result.
+#
+# See: http://stackoverflow.com/questions/3232943
 def updateDicRecursive(d, u):
     for k, v in u.items():
         if isinstance(v, collections.Mapping):
-            r = updateDicRecursive(d.get(k, {}), v)
-            d[k] = r
+            d[k] = updateDicRecursive(d.get(k, {}), v)
+        elif isinstance(v, collections.Sequence):
+            d[k] = list(d.get(k, [])) + list(v)
         else:
             d[k] = u[k]
     return d

commit d54bb2fb22931341423d6e048d7d199a4d948386

utils: fix updateDicRecursive regression
Matching on collections.Sequence is a bit too coarse as this applies
e.g. to strings too. We only really want to append lists.

This fixes a regression introduced by ce41dad9.

pym/bob/utils.py
@@ -78,8 +78,8 @@ def updateDicRecursive(d, u):
     for k, v in u.items():
         if isinstance(v, collections.Mapping):
             d[k] = updateDicRecursive(d.get(k, {}), v)
-        elif isinstance(v, collections.Sequence):
-            d[k] = list(d.get(k, [])) + list(v)
+        elif isinstance(v, list):
+            d[k] = d.get(k, []) + v
         else:
             d[k] = u[k]
     return d