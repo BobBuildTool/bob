c544935f64604f1f3cb2eff9d90e89948b1f2394
Jan Klötzke <jan@kloetzke.net>
2017-11-11 18:50:26 +0100

    bob: bump version to 0.13.0

--- pym/bob/__init__.py
+++ pym/bob/__init__.py
@@ -48,7 +48,7 @@ def getVersion():
 
     if not version:
         # Last fallback. See http://semver.org/ and adjust accordingly.
-        version = "0.13.0-rc2"
+        version = "0.13.0"
 
     return version
 
