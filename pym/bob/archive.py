896be5adc86e615e4ffcb49bca1214b52507f98b
Jan Klötzke <jan@kloetzke.net>
2017-11-21 17:39:13 +0100

    archive: add module description and behaviour

--- pym/bob/archive.py
+++ pym/bob/archive.py
@@ -14,6 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Binary archive handling module.
+
+Every backend is expected to implement the following behaviour:
+
+Downloads will almost never throw a fatal error. It should make no difference
+if an artifact was not found or could not be read. Bob should move on to the
+next backend or build the package. The only exception is when an artifact could
+be found and there are errors during extraction (e.g. some sanity checks fail,
+corrupted archive, no space left, ...).
+
+Uploads are supposed to throw a BuildError when something goes wrong unless the
+'nofail' option is set. If the artifact is already in the artifact repository
+it must not be overwritten. The backend should make sure that even on
+concurrent uploads the artifact must appear atomically for unrelated readers.
+"""
+
 from .errors import BuildError
 from .tty import colorize
 from .utils import asHexStr, removePath
