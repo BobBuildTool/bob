fc3783c2faa7d9a930770abb876e2ca592033e5e
Jan Klötzke <jan@kloetzke.net>
2018-03-27 21:14:56 +0200

    test: add rough test for plugin settings

---
+++ test/plugin-settings/plugins/settings.py
@@ -0,0 +1,19 @@
+from bob.input import PluginSetting
+
+class Settings(PluginSetting):
+    pass
+
+pluginSetting = Settings("default")
+
+def getSettings(args, **options):
+    return pluginSetting.getSettings()
+
+manifest = {
+    'apiVersion' : "0.13",
+    'stringFunctions' : {
+        "get-settings" : getSettings,
+    },
+    'settings' : {
+        "Settings" : pluginSetting,
+    },
+}
