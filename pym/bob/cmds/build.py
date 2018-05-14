BobBuildTool/bob

commit d31f9541261d7e8f9c3a4381ab24c34accd6756a

build: stabilize develop directory names
If the recipes change any directories that are still known are mapped
to the existing directories. Only unknown packages/variants are assigned
new directories. A nice side effect is that the directory mapping is
persisted and does not need to be calculated every time again.

pym/bob/cmds/build.py
@@ -21,6 +21,7 @@ import datetime
 import os
 import re
 import shutil
+import sqlite3
 import stat
 import subprocess
 import time
@@ -76,6 +77,139 @@ class LocalBuilderStatistic:
 
     def getActiveOverrides(self):
         return self.__activeOverrides
+
+class DevelopDirOracle:
+    """
+    Calculate directory names for develop mode.
+
+    If an external "persister" is used we just cache the calculated values. We
+    don't know it's behaviour and have to re-calculate everything from scratch
+    to be on the safe side.
+
+    The internal algorithm creates a separate directory for every recipe and
+    step variant. Only identical steps of the same recipe are put into the
+    same directory. In contrast to the releaseNamePersister() identical steps
+    of different recipes are put into distinct directories. If the recipes are
+    changed we keep existing mappings that still match the base directory.
+
+    Populating the database is done by traversing all packages and invoking the
+    name formatter for the visited packages. In case of the external persister
+    the result is directly cached. For the internal algorithm it has to be done
+    in two passes. The first pass collects all packages and their base
+    directories, possibly re-using old matches if possible. The second pass
+    assigns the final directory names to all other entries and writes them into
+    the database.
+    """
+
+    def __init__(self, formatter, externalPersister):
+        self.__formatter = formatter
+        self.__externalPersister = externalPersister(formatter) \
+            if externalPersister is not None else None
+        self.__dirs = {}
+        self.__known = {}
+        self.__ready = False
+
+    def __fmt(self, step, props):
+        key = step.getPackage().getRecipe().getName().encode("utf8") + step.getVariantId()
+
+        # Always look into the database first. We almost always need it.
+        self.__db.execute("SELECT dir FROM dirs WHERE key=?", (key,))
+        path = self.__db.fetchone()
+        path = path[0] if path is not None else None
+
+        # If we're ready we just interrogate the database.
+        if self.__ready:
+            assert path is not None, "{} missing".format(key)
+            return path
+
+        # If an external persistor is used we just call it and save the result.
+        if self.__externalPersister is not None:
+            path = self.__externalPersister(step, props)
+            self.__db.execute("INSERT INTO dirs VALUES (?, ?)", (key, path))
+            return path
+
+        # Try to find directory in database. If we find some the prefix has to
+        # match.
+        baseDir = self.__formatter(step, props)
+        if (path is not None) and path.startswith(baseDir):
+            self.__known[key] = path
+            return path
+
+        # Otherwise schedule for number assignment in next round by
+        # __writeBack(). The final path is not decided yet.
+        self.__dirs.setdefault(baseDir, []).append(key)
+
+    def __touch(self, step, done):
+        """Run through all dependencies and invoke name formatter.
+
+        Does it with "force sandbox" to catch also the workspaces that are
+        needed for build-id calculation.
+        """
+        key = step.getPackage().getRecipe().getName().encode("utf8") + step.getVariantId()
+        if key in done: return
+        done.add(key)
+        for d in step.getAllDepSteps(True):
+            if d.isValid(): self.__touch(d, done)
+        step.getWorkspacePath()
+
+    def __writeBack(self):
+        """Write calculated directories into database.
+
+        In case of an external persistor the data has already been written by
+        __fmt(). Otherwise we have to write the kept entries and calculate new
+        sub-directory numbers for new entries.
+        """
+        if self.__externalPersister is not None:
+            return
+
+        # clear all mappings
+        self.__db.execute("DELETE FROM dirs")
+
+        # write kept entries
+        self.__db.executemany("INSERT INTO dirs VALUES (?, ?)", self.__known.items())
+        knownDirs = set(self.__known.values())
+
+        # Add trailing number to new entries. Make sure they don't collide with
+        # kept entries...
+        for baseDir,keys in self.__dirs.items():
+            num = 1
+            for key in keys:
+                while True:
+                    path = os.path.join(baseDir, str(num))
+                    num += 1
+                    if path in knownDirs: continue
+                    self.__db.execute("INSERT INTO dirs VALUES (?, ?)", (key, path))
+                    break
+
+    def __openAndRefresh(self, cacheKey, rootPackageStep):
+        self.__db = db = sqlite3.connect(".bob-dev-dirs.sqlite3", isolation_level=None).cursor()
+        db.execute("CREATE TABLE IF NOT EXISTS meta(key PRIMARY KEY, value)")
+        db.execute("CREATE TABLE IF NOT EXISTS dirs(key PRIMARY KEY, dir)")
+
+        # Check if recipes were changed.
+        db.execute("BEGIN")
+        db.execute("SELECT value FROM meta WHERE key='vsn'")
+        vsn = db.fetchone()
+        if (vsn is None) or (vsn[0] != cacheKey):
+            if self.__externalPersister is not None:
+                db.execute("DELETE FROM dirs")
+            self.__touch(rootPackageStep, set())
+            self.__writeBack()
+            db.execute("INSERT OR REPLACE INTO meta VALUES ('vsn', ?)", (cacheKey,))
+            # Commit and start new read-only transaction
+            db.execute("END")
+            db.execute("BEGIN")
+
+    def prime(self, packages):
+        try:
+            self.__openAndRefresh(packages.getCacheKey(),
+                packages.getRootPackage().getPackageStep())
+        except sqlite3.Error as e:
+            raise BobError("Cannot save directory mapping: " + str(e))
+        self.__ready = True
+
+    def getFormatter(self):
+        return self.__fmt
 
 class LocalBuilder:
 
@@ -248,30 +382,6 @@ esac
         else:
             base = step.getPackage().getName()
         return os.path.join("dev", step.getLabel(), base.replace('::', os.sep))
-
-    @staticmethod
-    def developNamePersister(wrapFmt):
-        """Creates a separate directory for every recipe and step variant.
-
-        Only identical steps of the same recipe are put into the same
-        directory. In contrast to the releaseNamePersister() identical steps of
-        different recipes are put into distinct directories.
-        """
-        dirs = {}
-
-        def fmt(step, props):
-            baseDir = wrapFmt(step, props)
-            digest = (step.getPackage().getRecipe().getName(), step.getVariantId())
-            if digest in dirs:
-                res = dirs[digest]
-            else:
-                num = dirs.setdefault(baseDir, 0) + 1
-                res = os.path.join(baseDir, str(num))
-                dirs[baseDir] = num
-                dirs[digest] = res
-            return res
-
-        return fmt
 
     @staticmethod
     def makeRunnable(wrapFmt):
@@ -1213,18 +1323,6 @@ esac
         return r
 
 
-def touch(packages):
-    done = set()
-    def touchStep(step):
-        if step in done: return
-        done.add(step)
-        for d in step.getAllDepSteps():
-            if d.isValid(): touchStep(d)
-        step.getWorkspacePath()
-
-    touchStep(packages.getRootPackage().getPackageStep())
-
-
 def commonBuildDevelop(parser, argv, bobRoot, develop):
     parser.add_argument('packages', metavar='PACKAGE', type=str, nargs='+',
         help="(Sub-)package to build")
@@ -1292,7 +1390,7 @@ def commonBuildDevelop(parser, argv, bobRoot, develop):
     recipes = RecipeSet()
     recipes.defineHook('releaseNameFormatter', LocalBuilder.releaseNameFormatter)
     recipes.defineHook('developNameFormatter', LocalBuilder.developNameFormatter)
-    recipes.defineHook('developNamePersister', LocalBuilder.developNamePersister)
+    recipes.defineHook('developNamePersister', None)
     recipes.setConfigFiles(args.configFile)
     recipes.parse()
 
@@ -1324,15 +1422,14 @@ def commonBuildDevelop(parser, argv, bobRoot, develop):
 
     if develop:
         nameFormatter = recipes.getHook('developNameFormatter')
-        developPersister = recipes.getHook('developNamePersister')
-        nameFormatter = developPersister(nameFormatter)
+        developPersister = DevelopDirOracle(nameFormatter, recipes.getHook('developNamePersister'))
+        nameFormatter = developPersister.getFormatter()
     else:
         nameFormatter = recipes.getHook('releaseNameFormatter')
         nameFormatter = LocalBuilder.releaseNamePersister(nameFormatter)
     nameFormatter = LocalBuilder.makeRunnable(nameFormatter)
     packages = recipes.generatePackages(nameFormatter, defines, args.sandbox)
-    if develop:
-        touch(packages)
+    if develop: developPersister.prime(packages)
 
     builder = LocalBuilder(recipes, cfg.get('verbosity', 0) + args.verbose - args.quiet, args.force,
                            args.no_deps, True if args.build_mode == 'build-only' else False,
@@ -1444,7 +1541,7 @@ def doProject(argv, bobRoot):
 
     recipes = RecipeSet()
     recipes.defineHook('developNameFormatter', LocalBuilder.developNameFormatter)
-    recipes.defineHook('developNamePersister', LocalBuilder.developNamePersister)
+    recipes.defineHook('developNamePersister', None)
     recipes.setConfigFiles(args.configFile)
     recipes.parse()
 
@@ -1452,11 +1549,10 @@ def doProject(argv, bobRoot):
     envWhiteList |= set(args.white_list)
 
     nameFormatter = recipes.getHook('developNameFormatter')
-    developPersister = recipes.getHook('developNamePersister')
-    nameFormatter = developPersister(nameFormatter)
-    nameFormatter = LocalBuilder.makeRunnable(nameFormatter)
+    developPersister = DevelopDirOracle(nameFormatter, recipes.getHook('developNamePersister'))
+    nameFormatter = LocalBuilder.makeRunnable(developPersister.getFormatter())
     packages = recipes.generatePackages(nameFormatter, defines, sandboxEnabled=args.sandbox)
-    touch(packages)
+    developPersister.prime(packages)
 
     from ..generators.QtCreatorGenerator import qtProjectGenerator
     from ..generators.EclipseCdtGenerator import eclipseCdtGenerator
@@ -1533,7 +1629,7 @@ def doStatus(argv, bobRoot):
     recipes = RecipeSet()
     recipes.defineHook('releaseNameFormatter', LocalBuilder.releaseNameFormatter)
     recipes.defineHook('developNameFormatter', LocalBuilder.developNameFormatter)
-    recipes.defineHook('developNamePersister', LocalBuilder.developNamePersister)
+    recipes.defineHook('developNamePersister', None)
     recipes.setConfigFiles(args.configFile)
     recipes.parse()
 
@@ -1544,16 +1640,15 @@ def doStatus(argv, bobRoot):
         # Develop names are stable. All we need to do is to replicate build's algorithm,
         # and when we produce a name, check whether it exists.
         nameFormatter = recipes.getHook('developNameFormatter')
-        developPersister = recipes.getHook('developNamePersister')
-        nameFormatter = developPersister(nameFormatter)
+        developPersister = DevelopDirOracle(nameFormatter, recipes.getHook('developNamePersister'))
+        nameFormatter = developPersister.getFormatter()
     else:
         # Release names are taken from persistence.
         nameFormatter = LocalBuilder.releaseNameInterrogator
     nameFormatter = LocalBuilder.makeRunnable(nameFormatter)
 
     packages = recipes.generatePackages(nameFormatter, defines, not args.develop)
-    if args.develop:
-       touch(packages)
+    if args.develop: developPersister.prime(packages)
 
     def showStatus(package, recurse, verbose, done):
         checkoutStep = package.getCheckoutStep()
@@ -1712,7 +1807,7 @@ been executed or does not exist), the line is omitted.
     recipes = RecipeSet()
     recipes.defineHook('releaseNameFormatter', LocalBuilder.releaseNameFormatter)
     recipes.defineHook('developNameFormatter', LocalBuilder.developNameFormatter)
-    recipes.defineHook('developNamePersister', LocalBuilder.developNamePersister)
+    recipes.defineHook('developNamePersister', None)
     recipes.setConfigFiles(args.configFile)
     recipes.parse()
 
@@ -1737,8 +1832,8 @@ been executed or does not exist), the line is omitted.
         # Develop names are stable. All we need to do is to replicate build's algorithm,
         # and when we produce a name, check whether it exists.
         nameFormatter = recipes.getHook('developNameFormatter')
-        developPersister = recipes.getHook('developNamePersister')
-        nameFormatter = developPersister(nameFormatter)
+        developPersister = DevelopDirOracle(nameFormatter, recipes.getHook('developNamePersister'))
+        nameFormatter = developPersister.getFormatter()
     else:
         # Release names are taken from persistence.
         nameFormatter = LocalBuilder.releaseNameInterrogator
@@ -1746,8 +1841,7 @@ been executed or does not exist), the line is omitted.
 
     # Find roots
     packages = recipes.generatePackages(nameFormatter, defines, args.sandbox)
-    if args.dev:
-        touch(packages)
+    if args.dev: developPersister.prime(packages)
 
     # Loop through packages
     for p in args.packages:

pym/bob/pathspec.py
@@ -888,3 +888,6 @@ class PackageSet:
             thisPackage = nextPackages[step]
 
         return thisPackage
+
+    def getCacheKey(self):
+        return self.__cacheKey

test/swap-deps/run.sh
@@ -9,6 +9,7 @@ cleanup
 
 run_bob dev root -DORDER=ab
 V1="$(run_bob query-path --develop -DORDER=ab root/variant-a/dep)"
+rm -f .bob-dev-dirs.sqlite3
 run_bob dev root -DORDER=ba
 V2="$(run_bob query-path --develop -DORDER=ba root/variant-a/dep)"
 

commit a00308d4c357b270098d1848a6b646cb9cfcc342

build: fix path name calculation
There was a slight regression that got introduced in d31f9541 which
triggered an assertion that the workspace path was not in the database
even though it should have been there. The problem was that the
packages must be traversed on the package level to visit all packages
that are reachable by the query language.

pym/bob/cmds/build.py
@@ -107,6 +107,7 @@ class DevelopDirOracle:
             if externalPersister is not None else None
         self.__dirs = {}
         self.__known = {}
+        self.__visited = set()
         self.__ready = False
 
     def __fmt(self, step, props):
@@ -136,21 +137,32 @@ class DevelopDirOracle:
             return path
 
         # Otherwise schedule for number assignment in next round by
-        # __writeBack(). The final path is not decided yet.
-        self.__dirs.setdefault(baseDir, []).append(key)
+        # __writeBack(). The final path is not decided yet. Note that a key
+        # might still map to several directories. We make sure to take only
+        # the first one, though.
+        if key not in self.__visited:
+            self.__dirs.setdefault(baseDir, []).append(key)
+            self.__visited.add(key)
 
-    def __touch(self, step, done):
+    def __touch(self, package, done):
         """Run through all dependencies and invoke name formatter.
 
-        Does it with "force sandbox" to catch also the workspaces that are
-        needed for build-id calculation.
+        Traversal is done on package level to gather all reachable packages of
+        the query language.
         """
-        key = step.getPackage().getRecipe().getName().encode("utf8") + step.getVariantId()
+        key = package._getId()
         if key in done: return
         done.add(key)
-        for d in step.getAllDepSteps(True):
-            if d.isValid(): self.__touch(d, done)
-        step.getWorkspacePath()
+
+        # Traverse direct package dependencies only to keep the recipe order.
+        # Because we start from the root we are guaranteed to see all packages.
+        for d in package.getDirectDepSteps():
+            self.__touch(d.getPackage(), done)
+
+        # Calculate the paths of all steps
+        package.getPackageStep().getWorkspacePath()
+        package.getBuildStep().getWorkspacePath()
+        package.getCheckoutStep().getWorkspacePath()
 
     def __writeBack(self):
         """Write calculated directories into database.
@@ -181,7 +193,12 @@ class DevelopDirOracle:
                     self.__db.execute("INSERT INTO dirs VALUES (?, ?)", (key, path))
                     break
 
-    def __openAndRefresh(self, cacheKey, rootPackageStep):
+        # Clear intermediate variables to save memory.
+        self.__dirs = {}
+        self.__known = {}
+        self.__visited = set()
+
+    def __openAndRefresh(self, cacheKey, rootPackage):
         self.__db = db = sqlite3.connect(".bob-dev-dirs.sqlite3", isolation_level=None).cursor()
         db.execute("CREATE TABLE IF NOT EXISTS meta(key PRIMARY KEY, value)")
         db.execute("CREATE TABLE IF NOT EXISTS dirs(key PRIMARY KEY, dir)")
@@ -193,7 +210,7 @@ class DevelopDirOracle:
         if (vsn is None) or (vsn[0] != cacheKey):
             if self.__externalPersister is not None:
                 db.execute("DELETE FROM dirs")
-            self.__touch(rootPackageStep, set())
+            self.__touch(rootPackage, set())
             self.__writeBack()
             db.execute("INSERT OR REPLACE INTO meta VALUES ('vsn', ?)", (cacheKey,))
             # Commit and start new read-only transaction
@@ -203,7 +220,7 @@ class DevelopDirOracle:
     def prime(self, packages):
         try:
             self.__openAndRefresh(packages.getCacheKey(),
-                packages.getRootPackage().getPackageStep())
+                packages.getRootPackage())
         except sqlite3.Error as e:
             raise BobError("Cannot save directory mapping: " + str(e))
         self.__ready = True