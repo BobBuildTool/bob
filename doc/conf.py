842396708d3a2a7142c934b21c478577103e022b
Jan Klötzke <jan@kloetzke.net>
2018-10-07 14:37:43 +0200

    doc: replace deprecated sphinx.ext.pngmath module
    Fixes #216.

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ sys.path.insert(0, os.path.abspath('../pym'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.pngmath', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.imgmath', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
