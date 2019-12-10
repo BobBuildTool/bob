e1231ad7786e2fed7a582b1b6c6d73665900d4be
Jan Klötzke <Jan.Kloetzke@preh.de>
2019-12-10 15:00:50 +0100

    doc: strip '.dirty' when building on RTD
    Since the version number was normalized to PEP 440 the dirty marker is
    ".dirty" and not "-dirty" anymore.

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ except ImportError:
     # Strip "-dirty" if building on Read the Docs. They change conf.py and thus
     # always taint the repository.
     if os.environ.get('READTHEDOCS') == 'True':
-        if bobVersion.endswith('-dirty'):
+        if bobVersion.endswith('.dirty'):
             bobVersion = bobVersion[:-6]
 
 # The short X.Y version.
