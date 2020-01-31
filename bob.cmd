4d2942736e9ff7603bfa47f805cf8f21eb4e8dd7
Jan Klötzke <jan@kloetzke.net>
2020-01-31 15:04:14 +0100

    bob: add windows startup script
    Use cmd.exe script to start Bob from a development tree. If installed
    via pip the setuptools will generate appropriate startup scripts
    already.

---
+++ bob.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
+SET bobPath=%~dp0
+SET bobPath=%bobPath:~0,-1%
+
+set PYTHONPATH=%PYTHONPATH%;%bobPath%\pym
+python -c "from bob.scripts import bob; bob(r\"%bobPath%\")" %*
+
