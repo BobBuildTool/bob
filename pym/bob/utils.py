bc3f860eb51015faba821d88faf53119d8cd1521
Jan Klötzke <jan@kloetzke.net>
2020-02-16 13:35:56 +0100

    archive: distinguish symlink capability on Windows
    Some scripts and archive utilities will behave differently if it is
    possible to create symlinks on Windows or not. This capability is not
    granted by default but only available to administrative shells or user
    accounts that were given the SeCreateSymbolicLinkPrivilege privilege.
    Threat builds with and without the symlink capability differently
    because there there would be false sharing if symlinks are actually
    used.
    
    Fixes #303.

--- pym/bob/utils.py
+++ pym/bob/utils.py
@@ -5,7 +5,7 @@
 
 from .errors import BuildError, ParseError
 from binascii import hexlify
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 import collections.abc
 import hashlib
 import logging
@@ -179,6 +179,24 @@ def getPlatformTag():
         ret = b'm'
     else:
         ret = b''
+
+    # On Windows it depends on the SeCreateSymbolicLinkPrivilege capability if
+    # it is possible to create symlinks. Try to create a symlink to see if we
+    # have the privilege. Either the symlink() call fails directly or MSYS
+    # silently creates a copy (unless MSYS=winsymlinks:nativestrict is set).
+    if p in ('msys', 'cygwin', 'win32'):
+        canSymlink = False
+        try:
+            with TemporaryDirectory() as tmp:
+                with open(os.path.join(tmp, "file"), "w") as f:
+                    pass
+                canary = os.path.join(tmp, "canary")
+                os.symlink("file", canary)
+                canSymlink = os.path.islink(canary)
+        except OSError:
+            pass
+        if canSymlink:
+            ret += b'l'
 
     # cache result
     __platformTag = ret
