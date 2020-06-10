BobBuildTool/bob

commit 790c1be0a4d4883048535f9a804c743214ef443a

input: relax scmOverrides restrictions
The values of checkoutSCM keys are not only strings. They can be
booleans (e.g. sslVerify) and integers too (shallow). Allow any object
in the scmOverrides block and let the actual validation be done by the
schema checks.

Likewise allow to delete any key. Previously a partial list of supported
keys could be deleted but other valid keys could not.

pym/bob/input.py
@@ -2905,12 +2905,9 @@ class RecipeSet:
             "scmOverrides" : BuiltinSetting(
                 schema.Schema([{
                     schema.Optional('if') : schema.Or(str, IfExpression),
-                    schema.Optional('match') : schema.Schema({ str: str }),
-                    schema.Optional('del') : [
-                        "branch", "commit", "digestSHA1", "digestSHA256", "dir",
-                        "extract", "fileName", "if", "rev", "revision", "tag"
-                    ],
-                    schema.Optional('set') : schema.Schema({ str : str }),
+                    schema.Optional('match') : schema.Schema({ str: object }),
+                    schema.Optional('del') : str,
+                    schema.Optional('set') : object,
                     schema.Optional('replace') : schema.Schema({
                         str : schema.Schema({
                             'pattern' : str,

pym/bob/scm/scm.py
@@ -39,11 +39,12 @@ class ScmOverride:
         if self.__if is not None and not env.evaluate(self.__if, "scmOverride::if"): return False
         for (key, value) in self.__match.items():
             if key not in scm: return False
-            value = env.substitute(value, "svmOverride::match")
-            # the type of scm['if'] could be IfExpression and it is valid to compare the if keyword to match
-            # to a SCM. The old behavior of this is to compare the values as string rather than to evaluate it
-            # and compare the result. Casting the IfExpression to a str we do not change the behavior.
-            if not fnmatch.fnmatchcase(str(scm[key]), value): return False
+            if type(scm[key]) != type(value): return False
+            if isinstance(value, str):
+                value = env.substitute(value, "svmOverride::match")
+                if not fnmatch.fnmatchcase(scm[key], value): return False
+            else:
+                if scm[key] != value: return False
         return True
 
     def __hash__(self):
@@ -56,7 +57,10 @@ class ScmOverride:
 
     def __applyEnv(self, env):
         rm = [ env.substitute(d, "svmOverrides::del") for d in self.__del ]
-        set = { k : env.substitute(v, "svmOverrides::set: "+k) for (k,v) in self.__set.items() }
+        set = {
+            k : env.substitute(v, "svmOverrides::set: "+k) if isinstance(v, str) else v
+            for (k,v) in self.__set.items()
+        }
         return rm, set
 
     def mangle(self, scm, env):

commit bdf1ae81be5017164c61e82e85c491d2f241c01b

input: fix scmOverrides regression
The "del" property of a scmOverrides is a list of keys that should be
deleted. Commit 790c1be0 accidentally changed it schema so that it did
not match valid input anymore.

Fixes #348.

pym/bob/input.py
@@ -2956,7 +2956,7 @@ class RecipeSet:
                 schema.Schema([{
                     schema.Optional('if') : schema.Or(str, IfExpression),
                     schema.Optional('match') : schema.Schema({ str: object }),
-                    schema.Optional('del') : str,
+                    schema.Optional('del') : [str],
                     schema.Optional('set') : object,
                     schema.Optional('replace') : schema.Schema({
                         str : schema.Schema({