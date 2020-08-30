e8b83f41a0551d3e09c8c1f6076cf9c729d9f757
Jan Klötzke <jan@kloetzke.net>
2020-08-30 21:23:40 +0200

    test: verify that sandbox fingerprints are cached

--- test/sandbox-fingerprints/run.sh
+++ test/sandbox-fingerprints/run.sh
@@ -18,7 +18,12 @@ cat >default.yaml <<EOF
 archive:
     backend: file
     path: "$ARCHIVE"
+whitelist:
+    - FAIL_FINGERPRINT_IF_SET
 EOF
+
+# Test the test: must fail in the fingerprintScript
+FAIL_FINGERPRINT_IF_SET=1 expect_fail run_bob dev --sandbox --upload root
 
 # First build in the sandbox, upload and then verify that we can download it
 # directly in the non-sandbox build.
@@ -28,9 +33,13 @@ run_bob dev --download forced root
 
 # Verify that the sandbox build does not need the sandbox image but that the
 # fingerprint was cached.
-rm -rf dev/
-run_bob dev --sandbox --download forced root
+cleanup
+FAIL_FINGERPRINT_IF_SET=1 run_bob dev --sandbox --download forced root
 if [[ $(ls dev/dist/ | wc -l) -ne 1 ]] ; then
 	echo "Expected only one built package" >&2
 	exit 1
 fi
+
+# Verify that the fingerprint of the sandbox was cached locally.
+rm -rf dev
+FAIL_FINGERPRINT_IF_SET=1 run_bob dev --sandbox --download=no root
