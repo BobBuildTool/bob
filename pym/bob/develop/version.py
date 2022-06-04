0a1c3178c231ba1c6a3bcd33e1c708500099c35a
Jan Klötzke <jan@kloetzke.net>
2022-06-04 23:38:17 +0200

    version: bump to 0.21

--- pym/bob/develop/version.py
+++ pym/bob/develop/version.py
@@ -61,7 +61,7 @@ def getVersion():
 
     if not version:
         # Last fallback. See PEP 440 and adjust accordingly.
-        version = "0.20+unknown"
+        version = "0.21+unknown"
 
     return version
 
