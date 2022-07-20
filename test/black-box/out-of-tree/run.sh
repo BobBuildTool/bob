96aabbe7e023a127eb340cc4ff5084b2f4d0d56b
Jan Klötzke <jan@kloetzke.net>
2022-07-20 22:58:52 +0200

    test: do not compare paths literally
    When testing on Windows there is no single path representation inside
    MSYS. The following paths are equivalent for example:
    
      /c/Users/runneradmin/AppData/Local/Temp/tmp.xP6Dn5rkML/one/dev/dist/root/1/workspace
      /tmp/tmp.xP6Dn5rkML/one
    
    Because of this, remove the path check altogether.

--- test/black-box/out-of-tree/run.sh
+++ test/black-box/out-of-tree/run.sh
@@ -13,8 +13,6 @@ mkdir "$buildDir/one"
 pushd "$buildDir/one"
 run_bob init "$srdDir"
 run_bob dev root
-read -r resultDir < dev/dist/root/1/workspace/path.txt
-[[ $resultDir =~ ${buildDir}/one ]] || die "Result not in build dir"
 expect_output "default" cat dev/dist/root/1/workspace/setting.txt
 popd
 cmp recipes/input.txt "$buildDir/one/dev/dist/root/1/workspace/output.txt"
@@ -27,8 +25,6 @@ environment:
     SETTING: override
 EOF
 run_bob dev root
-read -r resultDir < dev/dist/root/1/workspace/path.txt
-[[ $resultDir =~ ${buildDir}/two ]] || die "Result not in build dir"
 # A default.yaml in the external build directory takes precedence
 expect_output "override" cat dev/dist/root/1/workspace/setting.txt
 popd
