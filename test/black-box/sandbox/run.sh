6f02df334e6191a6363602269f6bea15fc9f3238
Jan Klötzke <jan@kloetzke.net>
2024-04-20 17:42:49 +0200

    test: add tests for sandbox UID overrides
    Test that we can keep the UID or be root inside the sandbox.

--- test/black-box/sandbox/run.sh
+++ test/black-box/sandbox/run.sh
@@ -10,3 +10,7 @@ cleanup
 
 run_bob build -DFOO=bar root
 run_bob dev --sandbox -E root
+
+# Check that we can keep our UID or even be root inside sandbox
+run_bob dev --sandbox -c as-self -DEXPECT_UID=$UID root
+run_bob dev --sandbox -c as-root -DEXPECT_UID=0 root
