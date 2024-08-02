c16aa1a4968a5c1fb4f51390ff202d8f842d9673
Jan Klötzke <jan@kloetzke.net>
2024-08-02 17:48:00 +0200

    setup: require at least Python 3.7
    Python 3.6 is end-of-life since 2021-12-23. Even though 3.7 is also not
    supported any more, Debian oldoldstable is still using it. We don't gain
    much by dropping its support so let's keep it for the time being.

--- pym/bob/develop/version.py
+++ pym/bob/develop/version.py
@@ -9,8 +9,8 @@ import sys
 if sys.version_info.major != 3:
     print("Bob requires Python 3")
     sys.exit(1)
-elif sys.version_info.minor < 6:
-    print("Bob requires at least Python 3.6")
+elif sys.version_info.minor < 7:
+    print("Bob requires at least Python 3.7")
     sys.exit(1)
 
 def getVersion():
