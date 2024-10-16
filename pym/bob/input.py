BobBuildTool/bob

commit f70beb1fc176a1b6fc57fb22ca503f68aed74df4

layers: SCM support
Allow to specify scm properties for layers and use them to checkout /
update the layers. Only git, svn, cvs or url scms are supported.

Having layers with a SCMSpec they are only updated during bob dev|build
if --build-only is not set. Otherwise they are not touched by any
existing bob command.

In addition the new `bob-layers` command can be used to update layers
and show their status the same way like `bob status` does.

pym/bob/input.py
@@ -1718,6 +1718,36 @@ class ScmValidator:
                 None)
         return data
 
+class LayerSpec:
+    def __init__(self, name, scm=None):
+        self.__name = name;
+        self.__scm = scm
+
+    def getName(self):
+        return self.__name
+
+    def getScm(self):
+        return self.__scm
+
+class LayerValidator:
+    def __init__(self):
+        self.__scmValidator = ScmValidator({
+            'git' : GitScm.SCHEMA,
+            'svn' : SvnScm.SCHEMA,
+            'cvs' : CvsScm.SCHEMA,
+            'url' : UrlScm.SCHEMA})
+
+    def validate(self, data):
+        if isinstance(data,str):
+            return LayerSpec(data)
+        if 'name' not in data:
+            raise schema.SchemaMissingKeyError("Missing 'name' key in {}".format(data), None)
+        _data = data.copy();
+        name = _data.get('name')
+        del _data['name']
+
+        return LayerSpec (name, self.__scmValidator.validate(_data)[0])
+
 class VarDefineValidator:
     def __init__(self, keyword):
         self.__varName = re.compile(r'^[A-Za-z_][A-Za-z0-9_]*$')
@@ -1947,7 +1977,7 @@ class Recipe(object):
             "buildScript" : "true",
             "packageScript" : "true"
         }
-        ret = Recipe(recipeSet, recipe, [], "", ".", "", "", properties)
+        ret = Recipe(recipeSet, recipe, "", "", ".", "", "", properties)
         ret.resolveClasses(Env())
         return ret
 
@@ -2914,7 +2944,30 @@ class RecipeSet:
             schema.Optional('max_depth') : int,
         })
 
-    STATIC_CONFIG_SCHEMA = schema.Schema({
+    SCM_SCHEMA = ScmValidator({
+        'git' : GitScm.SCHEMA,
+        'svn' : SvnScm.SCHEMA,
+        'cvs' : CvsScm.SCHEMA,
+        'url' : UrlScm.SCHEMA,
+        'import' : ImportScm.SCHEMA,
+    })
+
+    STATIC_CONFIG_LAYER_SPEC = {
+        schema.Optional('layersWhitelist') : [str],
+        schema.Optional('layersScmOverrides') : schema.Schema([{
+            schema.Optional('match') : schema.Schema({ str: object }),
+            schema.Optional('del') : [str],
+            schema.Optional('set') : schema.Schema({ str: object }),
+            schema.Optional('replace') : schema.Schema({
+                str : schema.Schema({
+                    'pattern' : str,
+                    'replacement' : str
+                })
+            })
+        }])
+    }
+
+    STATIC_CONFIG_SCHEMA_SPEC = {
         schema.Optional('bobMinimumVersion') : str, # validated separately in preValidate
         schema.Optional('plugins') : [str],
         schema.Optional('policies') : schema.Schema(
@@ -2939,23 +2992,48 @@ class RecipeSet:
             },
             error="Invalid policy specified! Are you using an appropriate version of Bob?"
         ),
-        schema.Optional('layers') : [str],
+        schema.Optional('layers') : [LayerValidator()],
         schema.Optional('scriptLanguage',
                         default=ScriptLanguage.BASH) : schema.And(schema.Or("bash", "PowerShell"),
                                                                   schema.Use(ScriptLanguage)),
-    })
-
-    SCM_SCHEMA = ScmValidator({
-        'git' : GitScm.SCHEMA,
-        'svn' : SvnScm.SCHEMA,
-        'cvs' : CvsScm.SCHEMA,
-        'url' : UrlScm.SCHEMA,
-        'import' : ImportScm.SCHEMA,
-    })
+    }
 
     MIRRORS_SCHEMA = ScmValidator({
         'url' : UrlScm.MIRRORS_SCHEMA,
     })
+
+    POLICIES = {
+        'noUndefinedTools' : (
+            "0.17.3.dev57",
+            InfoOnce("noUndefinedTools policy not set. Included but undefined tools are not detected at parsing time.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#noundefinedtools for more information.")
+        ),
+        'scmIgnoreUser' : (
+            "0.17.3.dev97",
+            InfoOnce("scmIgnoreUser policy not set. Authentication part URL is tainting binary artifacts.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#scmignoreuser for more information.")
+        ),
+        'pruneImportScm' : (
+            "0.17.3.dev102",
+            InfoOnce("pruneImportScm policy not set. Incremental builds of 'import' SCM may lead to wrong results.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#pruneimportscm for more information.")
+        ),
+        'gitCommitOnBranch' : (
+            "0.21.0.dev5",
+            InfoOnce("gitCommitOnBranch policy not set. Will not check if commit / tag is on configured branch.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#gitcommitonbranch for more information.")
+        ),
+        'fixImportScmVariant' : (
+            "0.22.1.dev34",
+            InfoOnce("fixImportScmVariant policy not set. Recipe variant calculation w/ import SCM is boguous.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#fiximportscmvariant for more information.")
+        ),
+        "defaultFileMode": (
+            "0.24rc1",
+            InfoOnce("defaultFileMode policy not set. File mode of URL SCMs not set for locally copied files.",
+                help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#defaultfilemode for more information.")
+        ),
+    }
 
     _ignoreCmdConfig = False
     @classmethod
@@ -2992,38 +3070,7 @@ class RecipeSet:
         self.__uiConfig = {}
         self.__shareConfig = {}
         self.__layers = []
-        self.__policies = {
-            'noUndefinedTools' : (
-                "0.17.3.dev57",
-                InfoOnce("noUndefinedTools policy not set. Included but undefined tools are not detected at parsing time.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#noundefinedtools for more information.")
-            ),
-            'scmIgnoreUser' : (
-                "0.17.3.dev97",
-                InfoOnce("scmIgnoreUser policy not set. Authentication part URL is tainting binary artifacts.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#scmignoreuser for more information.")
-            ),
-            'pruneImportScm' : (
-                "0.17.3.dev102",
-                InfoOnce("pruneImportScm policy not set. Incremental builds of 'import' SCM may lead to wrong results.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#pruneimportscm for more information.")
-            ),
-            'gitCommitOnBranch' : (
-                "0.21.0.dev5",
-                InfoOnce("gitCommitOnBranch policy not set. Will not check if commit / tag is on configured branch.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#gitcommitonbranch for more information.")
-            ),
-            'fixImportScmVariant' : (
-                "0.22.1.dev34",
-                InfoOnce("fixImportScmVariant policy not set. Recipe variant calculation w/ import SCM is boguous.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#fiximportscmvariant for more information.")
-            ),
-            "defaultFileMode": (
-                "0.24rc1",
-                InfoOnce("defaultFileMode policy not set. File mode of URL SCMs not set for locally copied files.",
-                    help="See http://bob-build-tool.readthedocs.io/en/latest/manual/policies.html#defaultfilemode for more information.")
-            ),
-        }
+        self.__policies = RecipeSet.POLICIES.copy()
         self.__buildHooks = {}
         self.__sandboxOpts = {}
         self.__scmDefaults = {}
@@ -3383,7 +3430,8 @@ class RecipeSet:
         else:
             return schema[0].validate(default)
 
-    def parse(self, envOverrides={}, platform=getPlatformString(), recipesRoot=""):
+    def parse(self, envOverrides={}, platform=getPlatformString(), recipesRoot="",
+              noLayers=False):
         if not recipesRoot and os.path.isfile(".bob-project"):
             try:
                 with open(".bob-project") as f:
@@ -3393,17 +3441,19 @@ class RecipeSet:
         recipesDir = os.path.join(recipesRoot, "recipes")
         if not os.path.isdir(recipesDir):
             raise ParseError("No recipes directory found in " + recipesDir)
+        self.__projectRoot = recipesRoot or os.getcwd()
         self.__cache.open()
         try:
-            self.__parse(envOverrides, platform, recipesRoot)
+            self.__parse(envOverrides, platform, recipesRoot, noLayers)
         finally:
             self.__cache.close()
-        self.__projectRoot = recipesRoot or os.getcwd()
 
-    def __parse(self, envOverrides, platform, recipesRoot=""):
+    def __parse(self, envOverrides, platform, recipesRoot="", noLayers=False):
         if platform not in ('cygwin', 'darwin', 'linux', 'msys', 'win32'):
             raise ParseError("Invalid platform: " + platform)
         self.__platform = platform
+        self.__layers = []
+        self.__policies = RecipeSet.POLICIES.copy()
         self.__whiteList = set()
         if platform == 'win32':
             self.__whiteList |= set(["ALLUSERSPROFILE", "APPDATA",
@@ -3432,7 +3482,10 @@ class RecipeSet:
                 os.path.join(os.path.expanduser("~"), '.config')), 'bob', 'default.yaml'))
 
         # Begin with root layer
-        self.__parseLayer("", "9999", recipesRoot)
+        self.__parseLayer(LayerSpec(""), "9999", recipesRoot, noLayers)
+
+        if noLayers:
+            return
 
         # Out-of-tree builds may have a dedicated default.yaml
         if recipesRoot:
@@ -3475,14 +3528,17 @@ class RecipeSet:
         self.__rootRecipe = Recipe.createVirtualRoot(self, sorted(filteredRoots), self.__properties)
         self.__addRecipe(self.__rootRecipe)
 
-    def __parseLayer(self, layer, maxVer, recipesRoot):
+    def __parseLayer(self, layerSpec, maxVer, recipesRoot, noLayers=False):
+        layer = layerSpec.getName()
+
         if layer in self.__layers:
             return
         self.__layers.append(layer)
 
         rootDir = os.path.join(recipesRoot, os.path.join("layers", layer) if layer != "" else "")
         if not os.path.isdir(rootDir or "."):
-            raise ParseError(f"Layer '{layer}' does not exist!")
+            raise ParseError(f"Layer '{layer}' does not exist!",
+                                     help="You probably want to run 'bob layers update' to fetch missing layers.")
 
         configYaml = os.path.join(rootDir, "config.yaml")
         def preValidate(data):
@@ -3496,8 +3552,15 @@ class RecipeSet:
             if compareVersion(BOB_VERSION, minVer) < 0:
                 raise ParseError("Your Bob is too old. At least version "+minVer+" is required!")
 
-        config = self.loadYaml(configYaml, (RecipeSet.STATIC_CONFIG_SCHEMA, b''),
+        config_spec = {**RecipeSet.STATIC_CONFIG_SCHEMA_SPEC, **RecipeSet.STATIC_CONFIG_LAYER_SPEC}
+        config = self.loadYaml(configYaml, (schema.Schema(config_spec), b''),
             preValidate=preValidate)
+
+        # merge settings
+        for (name, value) in sorted([s for s in config.items() if s[0] in self.__settings],
+                                    key=lambda i: self.__settings[i[0]].priority):
+            self.__settings[name].merge(value)
+
         minVer = config.get("bobMinimumVersion", "0.16")
         if compareVersion(maxVer, minVer) < 0:
             raise ParseError("Layer '{}' requires a higher Bob version than root project!"
@@ -3505,6 +3568,7 @@ class RecipeSet:
         if compareVersion(minVer, "0.16") < 0:
             raise ParseError("Projects before bobMinimumVersion 0.16 are not supported!")
         maxVer = minVer # sub-layers must not have a higher bobMinimumVersion
+
 
         # Determine policies. The root layer determines the default settings
         # implicitly by bobMinimumVersion or explicitly via 'policies'. All
@@ -3519,6 +3583,9 @@ class RecipeSet:
                 for (name, (ver, warn)) in self.__policies.items() }
             for (name, behaviour) in config.get("policies", {}).items():
                 self.__policies[name] = (behaviour, None)
+
+        if noLayers:
+            return
 
         # First parse any sub-layers. Their settings have a lower precedence
         # and may be overwritten by higher layers.

pym/bob/cmds/build/build.py
@@ -9,6 +9,7 @@ from ...errors import BuildError
 from ...input import RecipeSet
 from ...intermediate import StepIR, PackageIR, RecipeIR, ToolIR, SandboxIR, \
     RecipeSetIR
+from ...layers import updateLayers
 from ...share import getShare
 from ...tty import setVerbosity, setTui, Warn
 from ...utils import copyTree, processDefines, EventLoopWrapper
@@ -171,6 +172,8 @@ def commonBuildDevelop(parser, argv, bobRoot, develop):
         help="Override default environment variable")
     parser.add_argument('-c', dest="configFile", default=[], action='append',
         help="Use config File")
+    parser.add_argument('-lc', dest="layerConfig", default=[], action='append',
+        help="Additional layer config")
     parser.add_argument('-e', dest="white_list", default=[], action='append', metavar="NAME",
         help="Preserve environment variable")
     parser.add_argument('-E', dest="preserve_env", default=False, action='store_true',
@@ -224,6 +227,9 @@ def commonBuildDevelop(parser, argv, bobRoot, develop):
         recipes.defineHook('developNameFormatter', LocalBuilder.developNameFormatter)
         recipes.defineHook('developNamePersister', None)
         recipes.setConfigFiles(args.configFile)
+        if args.build_mode != 'build-only':
+            setVerbosity(args.verbose)
+            updateLayers(recipes, loop, defines, args.verbose, args.attic, args.layerConfig)
         recipes.parse(defines)
 
         # if arguments are not passed on cmdline use them from default.yaml or set to default yalue

pym/bob/cmds/layers.py
@@ -1,0 +1,48 @@
+import argparse
+
+from ..input import RecipeSet
+from ..layers import Layers, updateLayers
+from ..tty import NORMAL, setVerbosity
+from ..utils import EventLoopWrapper, processDefines
+from .build.status import PackagePrinter
+
+def doLayers(argv, bobRoot):
+    parser = argparse.ArgumentParser(prog="bob layers", description='Handle layers')
+    parser.add_argument('action', type=str, choices=['update', 'status'], default="status",
+                        help="Action: [update, status]")
+    parser.add_argument('-c', dest="configFile", default=[], action='append',
+        help="Use config File.")
+    parser.add_argument('-lc', dest="layerConfig", default=[], action='append',
+        help="Additional layer config")
+    parser.add_argument('-v', '--verbose', default=NORMAL, action='count',
+        help="Increase verbosity (may be specified multiple times)")
+    parser.add_argument('-D', default=[], action='append', dest="defines",
+        help="Override default environment variable")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--attic', action='store_true', default=True,
+        help="Move scm to attic if inline switch is not possible (default).")
+    group.add_argument('--no-attic', action='store_false', default=None, dest='attic',
+        help="Do not move to attic, instead fail the build.")
+
+    args = parser.parse_args(argv)
+
+    setVerbosity(args.verbose)
+
+    defines = processDefines(args.defines)
+
+    with EventLoopWrapper() as (loop, executor):
+        recipes = RecipeSet()
+        recipes.setConfigFiles(args.configFile)
+        if args.action == "update":
+            updateLayers(recipes, loop, defines, args.verbose,
+                         args.attic, args.layerConfig)
+
+        recipes.parse(defines)
+
+        layers = Layers(recipes, loop, defines, args.attic)
+        layers.setLayerConfig(args.layerConfig)
+        layers.collect(False, args.verbose)
+        if args.action == "status":
+            pp = PackagePrinter(args.verbose, False, False)
+            layers.status(pp.show)
+

pym/bob/layers.py
@@ -1,0 +1,262 @@
+import datetime
+import os
+import schema
+import shutil
+from .errors import BuildError
+from .invoker import CmdFailedError, InvocationError, Invoker
+from .scm import ScmOverride
+from .state import BobState
+from .stringparser import Env
+from .input import RecipeSet, Scm, YamlCache
+from .utils import INVALID_CHAR_TRANS
+from .tty import DEBUG, EXECUTED, INFO, NORMAL, IMPORTANT, SKIPPED, WARNING, log
+
+class LayerStepSpec:
+    def __init__(self, path, whitelist):
+        self.__path = path
+        self.__whitelist = whitelist
+
+    @property
+    def workspaceWorkspacePath(self):
+        return self.__path
+
+    @property
+    def env(self):
+        return {}
+
+    @property
+    def envWhiteList(self):
+        return self.__whitelist
+
+class Layer:
+    def __init__(self, name, root, recipes, yamlCache, defines, attic, 
+                 whitelist, overrides, scm=None):
+        self.__attic = attic
+        self.__created = False
+        self.__defines = defines
+        self.__layerDir = os.path.join(root, "layers", name) if len(name) else root
+        self.__name = name
+        self.__recipes = recipes
+        self.__root = root
+        self.__subLayers = []
+        self.__scm = scm
+        self.__yamlCache = yamlCache
+        self.__whitelist = whitelist
+        self.__overrides = overrides
+
+    async def __checkoutTask(self, verbose):
+        if self.__scm is None:
+            return
+        dir = self.__scm.getProperties(False).get("dir")
+
+        invoker = Invoker(spec=LayerStepSpec(self.__layerDir, self.__whitelist),
+                          preserveEnv= False,
+                          noLogFiles = True,
+                          showStdOut = verbose > INFO,
+                          showStdErr = verbose > INFO,
+                          trace = verbose >= DEBUG,
+                          redirect=False, executor=None)
+        newState = {}
+        newState["digest"] = self.__scm.asDigestScript(),
+        newState["prop"] = {k:v for k,v in self.__scm.getProperties(False).items() if v is not None}
+
+        oldState = BobState().getLayerState(self.__layerDir)
+
+        if os.path.exists(self.__layerDir) and oldState is None:
+            raise BuildError(f"New layer checkout '{self.getName()}' collides with existing layer '{self.__layerDir}'!")
+
+        created = False
+        if not os.path.isdir(self.__layerDir):
+            os.makedirs(self.__layerDir)
+            self.__created = True
+
+        if not created \
+           and self.__scm.isDeterministic() \
+           and oldState is not None \
+           and oldState["digest"] == newState["digest"]:
+            log("CHECKOUT: Layer " +
+                "'{}' skipped (up to date)".format(self.getName()), SKIPPED, INFO)
+            return
+
+        if not created and oldState is not None and \
+            newState["digest"] != oldState["digest"]:
+
+            canSwitch = self.__scm.canSwitch(Scm(oldState["prop"],
+                                 Env(self.__defines),
+                                 overrides=self.__overrides,
+                                 recipeSet=self.__recipes))
+            if canSwitch:
+                log("SWITCH: Layer '{}' .. ok".format(self.getName()), EXECUTED, INFO)
+                ret = await invoker.executeScmSwitch(self.__scm, oldState["prop"])
+                if ret == 0:
+                    BobState().setLayerState(self.__layerDir, newState)
+                    return
+
+            if not self.__attic:
+                raise BuildError("Layer '{}' inline switch not possible and move to attic disabled '{}'!"
+                    .format(self.__name, self.__layerDir))
+            atticName = datetime.datetime.now().isoformat().translate(INVALID_CHAR_TRANS)+"_"+os.path.basename(self.__layerDir)
+            log("ATTIC: Layer " +
+                "{} (move to ../../layers.attic/{})".format(self.__layerDir, atticName), WARNING)
+            atticPath = os.path.join(self.__layerDir, "..", "..", "layers.attic")
+            if not os.path.isdir(atticPath):
+                os.makedirs(atticPath)
+            atticPath = os.path.join(atticPath, atticName)
+            os.rename(self.__layerDir, atticPath)
+            BobState().delLayerState(self.__layerDir)
+            os.makedirs(self.__layerDir)
+            self.__created = True
+
+        await self.__scm.invoke(invoker)
+        log("CHECKOUT: Layer " +
+                "'{}' .. ok".format(self.getName()), EXECUTED, NORMAL)
+        BobState().setLayerState(self.__layerDir, newState)
+
+    def checkout(self, loop, verbose):
+        try:
+            j = loop.create_task(self.__checkoutTask(verbose))
+            loop.run_until_complete(j)
+        except (CmdFailedError, InvocationError) as e:
+            if self.__created:
+                shutil.rmtree(self.__layerDir)
+            raise BuildError(f"Failed to checkout Layer {self.getName()}: {e.what}")
+
+    def getWorkspace(self):
+        return self.__layerDir
+
+    def getName(self):
+        return self.__name
+
+    def getWhiteList(self):
+        return self.__layerWhitelist
+
+    def loadYaml(self, path, schema):
+        if os.path.exists(path):
+            return self.__yamlCache.loadYaml(path, schema, {}, preValidate=lambda x: None)
+        return {}
+
+    def parse(self):
+        configYaml = os.path.join(self.__layerDir, "config.yaml")
+        config = self.loadYaml(configYaml, (schema.Schema({**RecipeSet.STATIC_CONFIG_SCHEMA_SPEC,
+                                                           **RecipeSet.STATIC_CONFIG_LAYER_SPEC}), b''))
+
+        self.__whitelist.append([c.upper() if self.__platform == "win32" else c
+            for c in config.get("layersWhiteList", []) ])
+
+        self.__overrides.extend([ ScmOverride(o) for o in config.get("layersScmOverrides", []) ])
+
+        for l in config.get('layers', []):
+            scmSpec = l.getScm()
+            if scmSpec is None: continue
+            scmSpec.update({'recipe':configYaml})
+            layerScms = Scm(scmSpec,
+                            Env(self.__defines),
+                            overrides=self.__overrides,
+                            recipeSet=self.__recipes)
+            self.__subLayers.append(Layer(l.getName(),
+                                          self.__root,
+                                          self.__recipes,
+                                          self.__yamlCache,
+                                          self.__defines,
+                                          self.__attic,
+                                          self.__whitelist,
+                                          self.__overrides,
+                                          layerScms))
+
+    def getSubLayers(self):
+        return self.__subLayers
+
+    def status(self, printer):
+        if self.__scm is None:
+            return
+        status = self.__scm.status(self.__layerDir)
+        printer(status, self.__layerDir)
+
+class Layers:
+    def __init__(self, recipes, loop, defines, attic):
+        self.__layers = {}
+        self.__loop = loop
+        self.__recipes = recipes
+        self.__attic = attic
+        self.__defines = defines
+        self.__yamlCache = YamlCache()
+        self.__whitelist = []
+        self.__overrides = []
+        self.__layerConfigFiles = []
+
+    def __haveLayer(self, layer):
+        for depth,layers in self.__layers.items():
+            for l in layers:
+                if l.getName() == layer.getName():
+                    return True
+        return False
+
+    def __collect(self, depth, update, verbose):
+        self.__layers[depth+1] = []
+        newLevel = False
+        for l in self.__layers[depth]:
+            if update:
+                l.checkout(self.__loop, verbose)
+            l.parse()
+            for subLayer in l.getSubLayers():
+                if not self.__haveLayer(subLayer):
+                    self.__layers[depth+1].append(subLayer)
+                    newLevel = True
+        if newLevel:
+            self.__collect(depth + 1, update, verbose)
+
+    def cleanupUnused(self):
+        old_layers = BobState().getLayers()
+        layers = []
+        for level in sorted(self.__layers.keys()):
+            for layer in self.__layers[level]:
+                layers.append(layer.getWorkspace())
+
+        for d in [x for x in old_layers if x not in layers]:
+            if os.path.exists(d):
+                atticName = datetime.datetime.now().isoformat().translate(INVALID_CHAR_TRANS)+"_"+os.path.basename(d)
+                log("ATTIC: Layer " +
+                    "{} (move to ../../layers.attic/{})".format(d, atticName), WARNING)
+                atticPath = os.path.join(d, "..", "..", "layers.attic")
+                if not os.path.isdir(atticPath):
+                    os.makedirs(atticPath)
+                atticPath = os.path.join(atticPath, atticName)
+                os.rename(d, atticPath)
+                BobState().delLayerState(d)
+
+    def collect(self, update, verbose=0):
+        self.__yamlCache.open()
+        try:
+            for c in self.__layerConfigFiles:
+                c += ".yaml"
+                if os.path.exists(c):
+                    config = self.__yamlCache.loadYaml(c, (schema.Schema(RecipeSet.STATIC_CONFIG_LAYER_SPEC), b''),
+                                                       {}, preValidate=lambda x: None)
+                    self.__whitelist.append([c.upper() if self.__platform == "win32" else c
+                        for c in config.get("layersWhiteList", []) ])
+                    self.__overrides.extend([ ScmOverride(o) for o in config.get("layersScmOverrides", []) ])
+                else:
+                    raise BuildError(f"Layer config file {c} not found" )
+
+            rootLayers = Layer("", os.getcwd(), self.__recipes, self.__yamlCache,
+                               self.__defines, self.__attic, self.__whitelist, self.__overrides)
+            rootLayers.parse()
+            self.__layers[0] = rootLayers.getSubLayers();
+            self.__collect(0, update, verbose)
+        finally:
+            self.__yamlCache.close()
+
+    def setLayerConfig(self, configFiles):
+        self.__layerConfigFiles = configFiles
+
+    def status(self, printer):
+        for level in sorted(self.__layers.keys()):
+            for layer in self.__layers[level]:
+                layer.status(printer)
+
+def updateLayers(recipes, loop, defines, verbose, attic, layerConfigs):
+    recipes.parse(defines, noLayers=True)
+    layers = Layers(recipes, loop, defines, attic)
+    layers.setLayerConfig(layerConfigs)
+    layers.collect(True, verbose)
+    layers.cleanupUnused()

pym/bob/scripts.py
@@ -59,6 +59,11 @@ def __ls(*args, **kwargs):
      doLS(*args, **kwargs)
      return 0
 
+def __layers(*args, **kwargs):
+     from .cmds.layers import doLayers
+     doLayers(*args, **kwargs)
+     return 0
+
 def __project(*args, **kwargs):
      from .cmds.build.project import doProject
      doProject(*args, **kwargs)
@@ -111,6 +116,7 @@ availableCommands = {
     "help"          : ('hl', __help, "Display help information about command"),
     "init"          : ('hl', __init, "Initialize build tree"),
     "jenkins"       : ('hl', __jenkins, "Configure Jenkins server"),
+    "layers"        : ('hl', __layers, "Handle layers"),
     "ls"            : ('hl', __ls, "List package hierarchy"),
     "project"       : ('hl', __project, "Create project files"),
     "show"          : ('hl', __show, "Show properties of a package"),

commit 0200430064d3507ed95f2214f0ed0c2e93ee6d7c

input: remove wrong merging of settings in config.yaml
This was accidentally introduced in f70beb1fc.

pym/bob/input.py
@@ -3566,12 +3566,6 @@ class RecipeSet:
         config_spec = {**RecipeSet.STATIC_CONFIG_SCHEMA_SPEC, **RecipeSet.STATIC_CONFIG_LAYER_SPEC}
         config = self.loadYaml(configYaml, (schema.Schema(config_spec), b''),
             preValidate=preValidate)
-
-        # merge settings
-        for (name, value) in sorted([s for s in config.items() if s[0] in self.__settings],
-                                    key=lambda i: self.__settings[i[0]].priority):
-            self.__settings[name].merge(value)
-
         minVer = config.get("bobMinimumVersion", "0.16")
         if compareVersion(maxVer, minVer) < 0:
             raise ParseError("Layer '{}' requires a higher Bob version than root project!"