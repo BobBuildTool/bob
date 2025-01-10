9328c237a68906e1f9b20fc053315fe20a732b3a
Ralf Hubert <rhubert@gmx.de>
2025-01-10 05:39:58 +0100

    url-scm: store downloaded files in separate directory
    Separate the downloaded files from the extracted files by downloading
    them into a `download` directory next to the workspace. Also the canary is
    generated there. The Gzip and XZ-Extractor always extract the files into
    the directory of the compressed file. Therefor the compressed files is
    copied into the workspace-directory first. By removing `-k`the
    compressed files are no longer kept.
    
    To trigger a attic move of old workspaces a version information is added
    to the url-scm spec.

--- test/black-box/extractors/run.sh
+++ test/black-box/extractors/run.sh
@@ -15,3 +15,18 @@ fi
 # Build and fetch result path
 run_bob dev -DINPUT_FILES="${INPUT}" -DIS_POSIX="$IS_POSIX" extract_test
 
+check_files() {
+  expect_not_exist dev/src/extract_test/1/workspace/$1/test.${2:-$1}.extracted
+  expect_not_exist dev/src/extract_test/1/workspace/$1/test.${2:-$1}
+  expect_exist dev/src/extract_test/1/download/$1/test.${2:-$1}.extracted
+  expect_exist dev/src/extract_test/1/download/$1/test.${2:-$1}
+}
+
+check_files "tar" "tgz"
+check_files "zip"
+
+if is_posix; then
+  check_files "gzip" "dat.gz"
+  check_files "xz" "dat.xz"
+  check_files "7z"
+fi
