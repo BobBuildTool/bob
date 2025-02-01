ff6bc935870dded4d23ffb8d9248e04ab647271b
Jan Klötzke <jan@kloetzke.net>
2025-02-01 22:50:38 +0100

    url: optimize extraction of gzip and xz files
    Instead of copying the input files, extract through stdout and redirect
    the output into the expected destination file. Unfortunately this
    requires to mirror the behaviour of gunzip and unxz wrt. file suffixes.

--- pym/bob/scm/url.py
+++ pym/bob/scm/url.py
@@ -168,13 +168,13 @@ class Extractor():
         self.strip = strip
         self.separateDownload = separateDownload
 
-    async def _extract(self, cmds, invoker):
+    async def _extract(self, cmds, invoker, stdout=None):
         destination = self.getCompressedFilePath(invoker)
         canary = destination+".extracted"
         if isYounger(destination, canary):
             for cmd in cmds:
                 if shutil.which(cmd[0]) is None: continue
-                await invoker.checkCommand(cmd, cwd=self.dir)
+                await invoker.checkCommand(cmd, cwd=self.dir, stdout=stdout)
                 invoker.trace("<touch>", canary)
                 with open(canary, "wb") as f:
                     pass
@@ -225,32 +225,57 @@ class ZipExtractor(Extractor):
         await self._extract(cmds, invoker)
 
 
-class GZipExtractor(Extractor):
+class SingleFileExtractor(Extractor):
 
     async def extract(self, invoker):
-        # gunzip extracts the file at the location of the input file. Copy the
-        # downloaded file to the workspace directory prio to uncompressing it
-        cmd = ["gunzip"]
-        if self.separateDownload:
-            shutil.copyfile(self.getCompressedFilePath(invoker),
-                            invoker.joinPath(self.dir, self.file))
+        # The gunzip and unxz tools extracts the file at the location of the
+        # input file. In case the separateDownload policy is active, the
+        # destination might be in a separete folder.
+        src = self.getCompressedFilePath(invoker)
+        dst = invoker.joinPath(self.dir, self.file)
+
+        suffix = dst[-4:]
+        if self.EXT_IGNORE_CASE:
+            suffix = suffix.lower()
+
+        for ext, repl in self.EXT_MAP.items():
+            if suffix.endswith(ext):
+                dst = dst[:-len(ext)] + repl
+                break
         else:
-            cmd.append("-k")
-        cmd.extend(["-f", self.file])
-        await self._extract([cmd], invoker)
+            raise BuildError("unkown suffix")
+
+        with open(dst, 'wb') as f:
+            cmd = [self.CMD, "-c", src]
+            await self._extract([cmd], invoker, f)
+
+        shutil.copystat(src, dst)
 
 
-class XZExtractor(Extractor):
+class GZipExtractor(SingleFileExtractor):
+    CMD = "gunzip"
+    EXT_IGNORE_CASE = True
+    EXT_MAP = {
+        ".gz" : "",
+        "-gz" : "",
+        ".z" : "",
+        "-z" : "",
+        "_z" : "",
+        ".tgz" : ".tar",
+        ".taz" : ".tar",
+    }
 
-    async def extract(self, invoker):
-        cmd = ["unxz"]
-        if self.separateDownload:
-            shutil.copyfile(self.getCompressedFilePath(invoker),
-                            invoker.joinPath(self.dir, self.file))
-        else:
-            cmd.append("-k")
-        cmd.extend(["-f", self.file])
-        await self._extract([cmd], invoker)
+
+class XZExtractor(SingleFileExtractor):
+    CMD = "unxz"
+    EXT_IGNORE_CASE = False
+    EXT_MAP = {
+        ".xz" : "",
+        ".lzma" : "",
+        ".lz" : "",
+        ".txz" : ".tar",
+        ".tlz" : ".tar",
+    }
 
 
 class SevenZipExtractor(Extractor):
