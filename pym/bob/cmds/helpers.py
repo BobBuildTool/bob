83bd2f6ffafa7831959c8468d3f727e83c136271
Jan Klötzke <jan@kloetzke.net>
2025-03-30 21:32:44 +0200

    helpers: move printing helpers here
    The show command supports various output formats. Move the helpers to a
    common location so that they can be reused for other commands too.

--- pym/bob/cmds/helpers.py
+++ pym/bob/cmds/helpers.py
@@ -7,3 +7,32 @@ def processDefines(defs):
         key, _sep, value = define.partition('=')
         defines[key] = value
     return defines
+
+
+def dumpYaml(doc, indent):
+    if indent is None:
+        style = None
+    else:
+        style = False
+
+    import yaml
+    return yaml.dump(doc, default_flow_style=style, indent=indent)
+
+def dumpJson(doc, indent):
+    import json
+    return json.dumps(doc, indent=indent, sort_keys=True)
+
+def dumpFlat(doc, prefix=""):
+    ret = []
+    if isinstance(doc, dict):
+        for k,v in sorted(doc.items()):
+            ret.extend(dumpFlat(v, prefix+"."+k if prefix else k))
+    elif isinstance(doc, list):
+        i = 0
+        for v in doc:
+            ret.extend(dumpFlat(v, "{}[{}]".format(prefix, i)))
+            i += 1
+    else:
+        ret = [ "{}={!r}".format(prefix, doc) ]
+
+    return ret
