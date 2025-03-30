ea27e2de048eb0d12b3ee21e2a39ca36723195b1
Jan Klötzke <jan@kloetzke.net>
2025-03-30 22:00:05 +0200

    layers: add an ls sub-command
    The 'bob layers ls' command lists all known layers. For each layer, the
    directory and the managed/unmanaged disposition is printed at least. For
    managed layers, the SCM properties are shown additonally.
    
    Because this command might be convenient for scripting, add different
    output formats. We support the same formats as 'bob show' here.
    
    Fixes #631.

--- pym/bob/cmds/layers.py
+++ pym/bob/cmds/layers.py
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import argparse
+import os
 
 from ..layers import Layers, updateLayers
 from ..tty import NORMAL, setVerbosity
 from ..utils import EventLoopWrapper
 from .build.status import PackagePrinter
-from .helpers import processDefines
+from .helpers import processDefines, dumpYaml, dumpJson, dumpFlat
 
 def addDefaultArgs(parser):
     parser.add_argument('-lc', dest="layerConfig", default=[], action='append',
@@ -54,10 +55,65 @@ def doLayersUpdate(argv):
         updateLayers(loop, defines, args.verbose, args.attic, args.layerConfig)
 
 
+def lsFlat(layers):
+    ret = []
+    for name, layer in sorted(layers.items()):
+        ret.append("[" + name + "]")
+        ret.extend(dumpFlat(layer))
+        ret.append("")
+
+    return "\n".join(ret)
+
+def doLayersLs(argv):
+    parser = argparse.ArgumentParser(prog="bob layers update",
+                                     description='List layers and their SCMs')
+    addDefaultArgs(parser)
+
+    ex = parser.add_mutually_exclusive_group()
+    ex.add_argument('--indent', type=int, default=4,
+        help="Number of spaces to indent (default: 4)")
+    ex.add_argument('--no-indent', action='store_const', const=None,
+        dest='indent', help="No indent. Compact format.")
+    parser.add_argument('--format', choices=['yaml', 'json', 'flat'],
+        default="yaml", help="Output format")
+
+    args = parser.parse_args(argv)
+    defines = processDefines(args.defines)
+
+    layers = Layers(defines, False)
+    layers.setLayerConfig(args.layerConfig)
+    layers.collect(None, False)
+
+    ls = {}
+    for layer in layers:
+        workspace = layer.getWorkspace()
+        # Unmanaged layers have absolute paths to the project root directory.
+        # Try to convert them to something readable.
+        if os.path.isabs(workspace):
+            cwd = os.getcwd()
+            if workspace.startswith(cwd):
+                workspace = workspace[len(cwd)+1:]
+
+        l = { "disposition" : "managed" if layer.isManaged() else "unmanaged",
+               "path" : workspace }
+        if layer.isManaged():
+            l["scm"] = layer.getScm().getProperties(False, True)
+
+        ls[layer.getName()] = l
+
+    if args.format == 'yaml':
+        print(dumpYaml(ls, args.indent))
+    elif args.format == 'json':
+        print(dumpJson(ls, args.indent))
+    else:
+        assert args.format == 'flat'
+        print(lsFlat(ls))
+
+
 def doLayers(argv, bobRoot):
     parser = argparse.ArgumentParser(prog="bob layers", description='Handle layers')
-    parser.add_argument('action', type=str, choices=['update', 'status'],
-                        help="Action: [update, status]")
+    parser.add_argument('action', type=str, choices=['ls', 'update', 'status'],
+                        help="Command action")
     parser.add_argument('args', nargs=argparse.REMAINDER,
                         help="Arguments for action")
 
@@ -66,3 +122,5 @@ def doLayers(argv, bobRoot):
         doLayersStatus(args.args)
     elif args.action == "update":
         doLayersUpdate(args.args)
+    elif args.action == "ls":
+        doLayersLs(args.args)
