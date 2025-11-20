BobBuildTool/bob

commit 58e0ce3c4e5d5be27e575bd59c72a9aa9c3bbb2f

archive: treat more socket errors as warnings
When downloading from an archive, the build should not fail because of
network errors. After all, the binary archive is just a cache that might
not provide an archive. We now treat any errors up to and including the
connection establishment as warnings. Previously, refused connections
were already treated that way.

For uploads the behaviour does not change. Any network errors are
treated as errors for uploads.

Fixes #660.

pym/bob/archive.py
@@ -29,6 +29,7 @@ from tempfile import mkstemp, NamedTemporaryFile, TemporaryFile, gettempdir
 import asyncio
 import concurrent.futures
 import concurrent.futures.process
+import errno
 import gzip
 import io
 import os
@@ -46,6 +47,16 @@ ARCHIVE_GENERATION = '-1'
 ARTIFACT_SUFFIX = ".tgz"
 BUILDID_SUFFIX = ".buildid"
 FINGERPRINT_SUFFIX = ".fprnt"
+
+SOFT_DOWNLOAD_ERRORS = {
+    'ECONNREFUSED', # Connection refused
+    'EHOSTDOWN',    # Host is down
+    'EHOSTUNREACH', # No route to host
+    'ENETDOWN',     # Network is down
+    'ENETUNREACH',  # Network is unreachable
+    'ETIMEDOUT',    # Connection timed out
+}
+
 
 def buildIdToName(bid):
     return asHexStr(bid) + ARCHIVE_GENERATION
@@ -449,13 +460,13 @@ class BaseArchive(TarHelper):
             return (False, self._namedErrorString("not found"), WARNING)
         except (ArtifactDownloadError, HttpDownloadError) as e:
             return (False, self._namedErrorString(e.reason), WARNING)
-        except ConnectionRefusedError:
-            return (False, self._namedErrorString("connection failed"), WARNING)
         except (socket.herror, socket.gaierror) as e:
             return (False, self._namedErrorString(str(e)), WARNING)
         except BuildError as e:
             raise
         except OSError as e:
+            if errno.errorcode[e.errno] in SOFT_DOWNLOAD_ERRORS:
+                return (False, self._namedErrorString(os.strerror(e.errno)), WARNING)
             raise BuildError(self._namedErrorString("Cannot download artifact: " + str(e)))
         except tarfile.TarError as e:
             raise BuildError(self._namedErrorString("Error extracting binary artifact: " + str(e)))
@@ -491,13 +502,13 @@ class BaseArchive(TarHelper):
             return (None, self._namedErrorString("not found"), WARNING)
         except (ArtifactDownloadError, HttpDownloadError) as e:
             return (None, self._namedErrorString(e.reason), WARNING)
-        except ConnectionRefusedError:
-            return (None, self._namedErrorString("connection failed"), WARNING)
         except (socket.herror, socket.gaierror) as e:
             return (None, self._namedErrorString(str(e)), WARNING)
         except BuildError as e:
             raise
         except OSError as e:
+            if errno.errorcode[e.errno] in SOFT_DOWNLOAD_ERRORS:
+                return (None, self._namedErrorString(os.strerror(e.errno)), WARNING)
             raise BuildError(self._namedErrorString("Cannot download file: " + str(e)))
         finally:
             # Restore signals to default so that Ctrl+C kills process. Needed

commit afef9fb998559dd8141e6769486bd0d5be02bd74

archive: fix soft errors on Windows
Apparently, there are multiple names for socket errors in the errno
module. For example, the ConnectionRefusedError is declared as
errno.ECONNREFUSED, as well as errno.WSAECONNREFUSED. The back-mapping
in errno.errorcode prefers the WSA-prefix, though, making it
incompatible with the Linux version.

Instead of using the error name, use the errno number directly.

Fixes: 58e0ce3c4 ("archive: treat more socket errors as warnings")

pym/bob/archive.py
@@ -49,12 +49,12 @@ BUILDID_SUFFIX = ".buildid"
 FINGERPRINT_SUFFIX = ".fprnt"
 
 SOFT_DOWNLOAD_ERRORS = {
-    'ECONNREFUSED', # Connection refused
-    'EHOSTDOWN',    # Host is down
-    'EHOSTUNREACH', # No route to host
-    'ENETDOWN',     # Network is down
-    'ENETUNREACH',  # Network is unreachable
-    'ETIMEDOUT',    # Connection timed out
+    errno.ECONNREFUSED, # Connection refused
+    errno.EHOSTDOWN,    # Host is down
+    errno.EHOSTUNREACH, # No route to host
+    errno.ENETDOWN,     # Network is down
+    errno.ENETUNREACH,  # Network is unreachable
+    errno.ETIMEDOUT,    # Connection timed out
 }
 
 
@@ -463,7 +463,7 @@ class BaseArchive(TarHelper):
             else:
                 break
 
-        if self.__ignoreDownloadErrors and errno.errorcode[err.errno] in SOFT_DOWNLOAD_ERRORS:
+        if self.__ignoreDownloadErrors and err.errno in SOFT_DOWNLOAD_ERRORS:
             return (softerror_return, self._namedErrorString(os.strerror(err.errno)), WARNING)
         raise BuildError(self._namedErrorString(message + ": " + str(err)))
 